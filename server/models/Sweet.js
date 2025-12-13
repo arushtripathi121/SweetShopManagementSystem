8c9065ba1de160d2921d209feb274bf25bfb972b
arushtripathi121 <arushtripathi121@gmail.com>
2025-12-13 15:44:00 +0530

    updated sweet model

--- server/models/Sweet.js
+++ server/models/Sweet.js
@@ -5,7 +5,8 @@ const sweetSchema = new mongoose.Schema(
         name: { type: String, required: true },
         category: { type: String, required: true },
         price: { type: Number, required: true },
-        quantity: { type: Number, required: true }
+        quantity: { type: Number, required: true },
+        image: { type: String, required: true }
     },
     { timestamps: true }
 );
