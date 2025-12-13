d2e8a33f55480ab84c81f0a0fd2fa57b6eebac61
arushtripathi121 <arushtripathi121@gmail.com>
2025-12-13 16:09:14 +0530

    update sweet model

--- server/models/Sweet.js
+++ server/models/Sweet.js
@@ -6,7 +6,20 @@ const sweetSchema = new mongoose.Schema(
         category: { type: String, required: true },
         price: { type: Number, required: true },
         quantity: { type: Number, required: true },
-        image: { type: String, required: true }
+        image: { type: String, required: true },
+
+        rating: {
+            type: Number,
+            min: 0,
+            max: 5,
+            default: 4.5
+        },
+
+        description: {
+            type: String,
+            required: false,
+            default: "A delicious handcrafted sweet prepared using premium ingredients."
+        }
     },
     { timestamps: true }
 );
