6ebe0529f0418f61f4808fe840c1a05c6e503f81
arushtripathi121 <arushtripathi121@gmail.com>
2025-12-14 12:57:55 +0530

    update auth context

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext(null);
+export { AuthContext };
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
